1b3260b2f5d0008faf7747e982f3e081d4cadb88
Matt Deaver <mattrdeaver@gmail.com>
2016-06-11 10:27:33 -0400

    added gitignore

--- old/test.py
+++
@@ -1,6 +0,0 @@
-import urllib2
-from scrapy.selector import Selector
-from scrapy.http import HtmlResponse
-
-
-/html/body/center/table[3]/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table[3]/tbody/tr/td/table/tbody/tr/td[4]
